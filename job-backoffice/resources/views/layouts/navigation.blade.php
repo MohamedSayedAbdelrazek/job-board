1d38522013e3bc4f45a818a8dbadd643d0e9ef97
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-06-21 12:43:21 +0300

    fix navigation links

--- job-backoffice/resources/views/layouts/navigation.blade.php
+++ job-backoffice/resources/views/layouts/navigation.blade.php
@@ -13,23 +13,23 @@
             {{ __('Dashboard') }}
         </x-nav-link>
 
-        <x-nav-link :href="route('company.index')" :active="request()->routeIs('company.*')">
+        <x-nav-link :href="route('companies.index')" :active="request()->routeIs('companies.*')">
             {{ __('Companies') }}
         </x-nav-link>
 
-        <x-nav-link :href="route('application.index')" :active="request()->routeIs('application.*')">
-            {{ __('Applications') }}
+        <x-nav-link :href="route('job-applications.index')" :active="request()->routeIs('job-applications.*')">
+            {{ __('Job Applications') }}
         </x-nav-link>
 
-        <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.*')">
-            {{ __('Categories') }}
+        <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
+            {{ __('Job Categories') }}
         </x-nav-link>
 
-        <x-nav-link :href="route('job-vacancy.index')" :active="request()->routeIs('job-vacancy.*')">
+        <x-nav-link :href="route('job-vacancies.index')" :active="request()->routeIs('job-vacancies.*')">
             {{ __('Job Vacancies') }}
         </x-nav-link>
 
-        <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.*')">
+        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
             {{ __('Users') }}
         </x-nav-link>
         <hr />
--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
-
-
-
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
 
 Route::middleware('auth')->group(function () {
+    Route::get('/',[DashboardController::class,'index'])->name('dashboard');
+    Route::resource('categories',JobCategoryController::class);
+    Route::resource('companies',CompanyController::class);
+    Route::resource('job-vacancies',JobVacancyController::class);
+    Route::resource('job-applications',JobApplicationController::class);
+    Route::resource('users', UserController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
