a46d6d5587d79b5e7d8e18fa04f8ef7ea271e988
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-06-22 22:47:48 +0300

    Show all companies page

--- job-backoffice/app/Http/Controllers/CompanyController.php
+++ job-backoffice/app/Http/Controllers/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -9,10 +10,17 @@ class CompanyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
-        return view('companies.index');
+          $query=Company::latest(); //
+
+        if($request->has("archived")){
+            $query->onlyTrashed();
+        }
+
+        $companies=$query->paginate(10)->onEachSide(1);
+        return view('companies.index',compact('companies'));
     }
 
     /**
--- job-backoffice/resources/views/companies/index.blade.php
+++ job-backoffice/resources/views/companies/index.blade.php
@@ -1,16 +1,98 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Companies') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="text-2xl font-bold text-gray-800">
+
+            📁Companies{{ request()->input('archived')==true?'(Archived)':'' }}
+            </h2>
+            <div>
+                
+            @if (request()->input('archived')==true)
+                <a href="{{ route('companies.index') }}" class="inline-block bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-gray-700 transition">
+                    Active Companies
+                </a> 
+            @else
+                <a href="{{ route('companies.index',['archived'=>true]) }}" class="inline-block bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 transition">
+                    Archived Companies
+                </a>
+            @endif
+            
+           
+            
+            <a href="{{ route('companies.create') }}" class="inline-block bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 transition">
+                ➕ Add New Company
+            </a>
+
+            </div>
+            
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Companies") }}
-                </div>
+    {{-- Success Message --}}
+        <x-toast-notification/>
+        
+    <div class="p-6 bg-gray-100 min-h-screen">
+        <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+            <table class="min-w-full table-auto">
+                <thead class="bg-gradient-to-r from-indigo-500 to-blue-500 text-black">
+                    <tr>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Company Name</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Address</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Indusry</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Website</th>
+                         <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Owner</th> 
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($companies as $company)
+                        <tr class="border-b hover:bg-gray-50 transition">
+                            <td class="px-6 py-4 text-gray-800 font-medium">{{ $company->name }}</td>
+                            <td class="px-6 py-4 text-gray-800 font-medium">{{ $company->address }}</td>
+                            <td class="px-6 py-4 text-gray-800 font-medium">{{ $company->industry }}</td>
+                            <td class="px-6 py-4 text-gray-800 font-medium"><a target="_blank" href="{{ $company->website }}">{{ $company->website }}</a></td>
+                            <td class="px-6 py-4 text-gray-800 font-medium">{{ $company->owner->name }}</td>
+                            <td class="px-6 py-4 space-x-2">
+                                @if (request()->input('archived')==true)
+
+                                   <form method="POST" action="{{ route('companies.restore', $company->id) }}" class="inline-block">
+                                    @csrf
+                                    @method('put')
+                                    <button type="submit" onclick="return confirm('Are you sure you want to restore this category?')" class="bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-green-200 transition">
+                                        🔄 Restore
+                                    </button>
+
+                                @else
+                                <a href="{{ route('companies.show', $company->id) }}" class="inline-block bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-green-200 transition">
+                                    🧐 Show
+                                    </a>
+
+                                    <a href="{{ route('companies.edit', $company->id) }}" class="inline-block bg-yellow-100 text-yellow-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-yellow-200 transition">
+                                    ✍️ Edit
+                                    </a>
+
+                                     <form method="POST" action="{{ route('companies.destroy', $company->id) }}" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" onclick="return confirm('Are you sure you want to archive this category?')" class="bg-red-100 text-red-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-red-200 transition">
+                                        🗃️ Archive
+                                    </button>
+                                </form>
+
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="2" class="text-center text-gray-500 py-6">No Companies found.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{-- Pagination --}}
+            <div class="mt-4 px-4">
+                {{ $companies->links() }}
             </div>
         </div>
     </div>
