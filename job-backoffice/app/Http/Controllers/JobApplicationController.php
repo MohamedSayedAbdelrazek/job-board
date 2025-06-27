bf621ee45252198aa6192b33328338cd72bb8ea5
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-06-27 16:05:17 +0300

    Show All jobApplications is Done

--- job-backoffice/app/Http/Controllers/JobApplicationController.php
+++ job-backoffice/app/Http/Controllers/JobApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class JobApplicationController extends Controller
@@ -9,10 +10,16 @@ class JobApplicationController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
-        return view('job-applications.index');
+        $query=JobApplication::latest();
+        
+        if($request->has("archived")){
+            $query->onlyTrashed();
+        }
+        $jobApplications=$query->paginate(10)->onEachSide(1);
+        return view('job-applications.index',compact('jobApplications'));
     }
 
     /**
--- job-backoffice/resources/views/job-applications/index.blade.php
+++ job-backoffice/resources/views/job-applications/index.blade.php
@@ -1,16 +1,120 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Job Applications') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="text-2xl font-bold text-gray-800">
+
+            📁Job Applications{{ request()->input('archived')==true?'(Archived)':'' }}
+            </h2>
+            <div>
+                
+            @if (request()->input('archived')==true)
+                <a href="{{ route('job-applications.index') }}" class="inline-block bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-gray-700 transition">
+                    Active Applications
+                </a> 
+            @else
+                <a href="{{ route('job-applications.index',['archived'=>true]) }}" class="inline-block bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 transition">
+                    Archived  Applications
+                </a>
+            @endif
+        
+            </div>
+            
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Job Applications") }}
-                </div>
+        {{-- Success Message --}}
+        <x-toast-notification/>
+
+
+              
+    <div class="p-6 bg-gray-100 min-h-screen">
+        <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+            <table class="min-w-full table-auto">
+                <thead class="bg-gradient-to-r from-indigo-500 to-blue-500 text-black">
+                    <tr>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Applicant Name</th>
+                         <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Position (Job Vacancy)</th> 
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Company </th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Status</th>
+                        <th class="px-6 py-3 text-left text-sm font-semibold uppercase">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($jobApplications as $jobApplication)
+                        <tr class="border-b hover:bg-gray-50 transition">
+                            
+                            <td class="px-6 py-4 text-gray-800 font-medium">
+{{--  @TODO => when We can show the applicant account
+                                 @if (request()->input('archived')==true) --}}
+
+                                    <span class="text-gray-800">{{ $jobApplication->user->name }}</span>
+
+                                {{--   @else
+                                    <a class="text-blue-500 hover:text-blue-700 underline"  href="{{ route('job-vacancies.show',$job->id) }}">{{ $job->title }}</a>
+                                @endif 
+                                --}}
+                            </td>
+                            
+                              <td class="px-6 py-4 text-gray-800 font-medium">
+                                @if ($jobApplication->jobVacancy->title)
+                                      <a target="_blank" href="{{ route('job-vacancies.show',$jobApplication->jobVacancy->id)}}">{{$jobApplication->jobVacancy->title }}</a>
+                                @else
+                                     <span class="text-gray-500 italic">N/A</span>
+                                @endif
+                            </td>
+
+
+                            <td class="px-6 py-4 text-gray-800 font-medium">
+                                @if ($jobApplication->jobVacancy->company->name)
+                                      <a target="_blank" href="{{ route('companies.show',$jobApplication->jobVacancy->company->id)}}">{{$jobApplication->jobVacancy->company->name }}</a>
+                                @else
+                                     <span class="text-gray-500 italic">N/A</span>
+                                @endif
+                            </td>
+
+                            <td class="px-6 py-4 text-gray-800 font-medium">{{ $jobApplication->status }}</td>
+                            
+                            <td class="px-6 py-4 space-x-2">
+                                @if (request()->input('archived')==true)
+
+                                   <form method="POST" action="{{ route('job-applications.restore', $jobApplication->id) }}" class="inline-block">
+                                    @csrf
+                                    @method('put')
+                                    <button type="submit" onclick="return confirm('Are you sure you want to restore this  Job application?')" class="bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-green-200 transition">
+                                        🔄 Restore
+                                    </button>
+
+                                @else
+                                <a href="{{ route('job-applications.show', $jobApplication->id) }}" class="inline-block bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-green-200 transition">
+                                    🧐 Show
+                                    </a>
+
+                                    <a href="{{ route('job-applications.edit', $jobApplication->id) }}" class="inline-block bg-yellow-100 text-yellow-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-yellow-200 transition">
+                                    ✍️ Edit
+                                    </a>
+
+                                     <form method="POST" action="{{ route('job-applications.destroy', $jobApplication->id) }}" class="inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" onclick="return confirm('Are you sure you want to archive this category?')" class="bg-red-100 text-red-800 px-3 py-1 rounded-full text-sm font-semibold hover:bg-red-200 transition">
+                                        🗃️ Archive
+                                    </button>
+                                </form>
+
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="2" class="text-center text-gray-500 py-6">No Applications found.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{-- Pagination --}}
+            <div class="mt-4 px-4">
+                {{ $jobApplications->links() }}
             </div>
         </div>
     </div>
