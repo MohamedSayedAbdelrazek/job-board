1298a12e8d3a94632d9768bd1f84ca75787772ac
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-07-15 15:25:54 +0300

    change the title for both applications

--- job-app/resources/views/layouts/main.blade.php
+++ job-app/resources/views/layouts/main.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ?? 'Shaghalni' }}</title>
+    <title>{{ $title ?? 'HireMe – Job Portal' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Alpine.js Cloak -->
     <style>
